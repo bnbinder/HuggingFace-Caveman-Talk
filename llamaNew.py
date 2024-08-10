a81a0f0a16d23d2a6f0e3975869913491efa551d
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2024-08-10 00:19:21 -0400

    first version

--- llamaNew.py
+++ llamaNew.py
@@ -2,9 +2,11 @@ import torch
 import transformers
 from huggingface_hub import login
 import spacy
+import re
 
-login("hhhhh")
+login("eeee")
 nlp = spacy.load("en_core_web_sm")
+
 class Llama3:
     def __init__ (self, modelPath):
         self.modelID = modelPath
@@ -41,61 +43,41 @@ class Llama3:
             userInput = input("Ask the AI to write something or write \"theme\" and a theme after for it to generate some sentences based on the theme: ")
             response = ""
             if(userInput.lower().replace("\"", "")[:5] == "theme"):
-                response = self.getResponse("Using this theme, generate at least five sentences in paragraph form: " + userInput)
+                response = self.getResponse("Using this theme, generate at least five sentences in paragraph form. Do not include any introduction sentence responding to my prompt, only respond with the sentences generated. So something like \"Here are five sentences in paragraph form:\" do not include: " + userInput)
             else:
-                response = self.getResponse(userInput)
+                response = self.getResponse("Do not include any introduction sentence responding to my prompt, only respond with the sentences generated. So something like \"Here are five sentences in paragraph form:\" do not include: " + userInput)
             print("What Llama cooked up: " + response)
             yorn = input("Is this good (Yes/No)").lower()[0]
             if(yorn == "y"):
                 return response
     
-def caveManModify ():
-    while True:
-        output = """i'm going to the supermarket. will it be a good trip? 
-        i don't know for sure, but i'll try my best"""
-        nlp = spacy.load("en_core_web_sm")
-        doc = nlp(output)
-        for token in doc:
-            if token.tag_ == "VBP" or token.tag_ == "MD":
-                output = output.replace(token.text, "", 1)
-                continue
-            if token.dep_ == "det":
-                output = output.replace(token.text, "", 1)
-                #convdert these to token.methods so they dont screw up likke string methods do
-                continue
-            output = output.replace(token.text, token.lemma_, 1)
-            #print(token.lemma_)
-            print(f"Text: {token.text}\n"
-            f"Lemma: {token.lemma_}\n"
-            f"POS: {token.pos_}\n"
-            f"Tag: {token.tag_}\n"
-            f"Dep: {token.dep_}\n"
-            f"Shape: {token.shape_}\n"
-            f"Is Alpha: {token.is_alpha}\n"
-            f"Is Stop: {token.is_stop}\n"
-            f"Is Punct: {token.is_punct}\n"
-            f"Like Num: {token.like_num}\n"
-            f"Ent IOB: {token.ent_iob_}\n"
-            f"Ent Type: {token.ent_type_}\n")
-        print(output)
-        break
+    def caveManModify (self):
+        while True:
+            words = []
+            output = self.generateSentences()
+            doc = nlp(output)
+            for token in doc:
+                if token.tag_ == "VBP" or token.tag_ == "MD":
+                    continue
+                if token.dep_ == "det":
+                    continue
+                if token.text == "my":
+                    continue
+                if token.pos_ == "ADJ" and token.text != token.lemma_[:len(token.text) - 1]:
+                    words.append(token.text)
+                    continue
+                words.append(token.lemma_)
+            word = []
+            word.append(' '.join(words).capitalize())
+            simpWord = ' '.join(word)
+            simpWord = re.sub(r'\s+', ' ', simpWord)
+            simpWord = re.sub(r' \.', '.', simpWord)
+            simpWord = re.sub(r' \?', '?', simpWord)
+            simpWord = re.sub(r' \,', ',', simpWord)
+            simpWord = simpWord.strip()
+            print(simpWord)
+            break
 
 if __name__ == "__main__":
-    #bot = Llama3("meta-llama/Meta-Llama-3-8B-Instruct")
-    caveManModify()
-    
-"""
-
-Text: 'm, Lemma: be, POS: AUX, Tag: VBP, Dep: aux, Shape: 'x, Is Alpha: False, Is Stop: True, Is Punct: False, Like Num: False, Ent IOB: O, 
-Ent Type:
-
-Text: be, Lemma: be, POS: AUX, Tag: VB, Dep: ROOT, Shape: xx, Is Alpha: True, Is Stop: True, Is Punct: False, Like Num: False, Ent IOB: O, Ent Type:
-
-
-
-Text: i, Lemma: I, POS: PRON, Tag: PRP, Dep: nsubj, Shape: x, Is Alpha: True, Is Stop: True, 
-Is Punct: False, Like Num: False, Ent IOB: O, Ent Type:
-
-Text: it, Lemma: it, POS: PRON, Tag: PRP, Dep: nsubj, Shape: xx, Is Alpha: True, Is Stop: True, 
-Is Punct: False, Like Num: False, Ent IOB: O, Ent Type:
-"""
+    bot = Llama3("meta-llama/Meta-Llama-3-8B-Instruct")
+    bot.caveManModify()
